45729b74ded39c5062cafeed7cf5d9587f5af36f
Mustafa Dalga <mustafadalgaa@gmail.com>
2020-06-17 23:41:21 +0300

    Ekran görüntüsü alma scripti tamamlandı

---
+++ Ekran-goruntusu-alma/script.ps1
@@ -0,0 +1,21 @@
+[Reflection.Assembly]::LoadWithPartialName("System.Drawing")
+function screenshot([Drawing.Rectangle]$bounds, $path) {
+   $bmp = New-Object Drawing.Bitmap $bounds.width, $bounds.height
+   $graphics = [Drawing.Graphics]::FromImage($bmp)
+
+   $graphics.CopyFromScreen($bounds.Location, [Drawing.Point]::Empty, $bounds.size)
+
+   $bmp.Save($path)
+
+   $graphics.Dispose()
+   $bmp.Dispose()
+}
+
+$bounds = [Drawing.Rectangle]::FromLTRB(0, 0, 1366, 768)
+
+$dizin=Get-Location
+
+$isim="$(get-date -f yyyy-MM-dd-hh-mm-ss).png"
+
+
+screenshot $bounds "$dizin\$isim"
