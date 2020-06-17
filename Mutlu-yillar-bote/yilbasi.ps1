3a91924cad5f4a72c5e8f011d6668e3ae471c88a
Mustafa Dalga <mustafadalgaa@gmail.com>
2020-06-17 23:56:20 +0300

    Rastgele renklerden oluşan dinamik yıl başı ağaçlı yazı yazma scripti tamamlandı

---
+++ Mutlu-yillar-bote/yilbasi.ps1
@@ -0,0 +1,70 @@
+$colors = "Cyan","Green","Yellow","Red","Magenta","White"
+Clear-Host
+Write-Host
+ 
+While($true)
+{
+    $oldpos = $host.ui.RawUI.CursorPosition
+    Write-Host "        *" -ForegroundColor ($colors | get-random)
+    # Row One
+    Write-Host "       *" -ForegroundColor ($colors | get-random) -nonewline
+    Write-Host "*"        -ForegroundColor DarkGreen              -nonewline
+    Write-Host "*"        -ForegroundColor ($colors | get-random)
+    # Row Two
+    Write-Host "      *" -ForegroundColor ($colors | get-random) -nonewline
+    Write-Host "*"       -ForegroundColor DarkGreen              -nonewline
+    Write-Host "*"       -ForegroundColor ($colors | get-random) -nonewline
+    Write-Host "*"       -ForegroundColor DarkGreen              -nonewline
+    Write-Host "*"       -ForegroundColor ($colors | get-random)
+    # Row Three
+    Write-Host "     *"  -ForegroundColor ($colors | get-random) -nonewline
+    Write-Host "*"       -ForegroundColor DarkGreen              -nonewline
+    Write-Host "*"       -ForegroundColor ($colors | get-random) -nonewline
+    Write-Host "*"       -ForegroundColor DarkGreen              -nonewline
+    Write-Host "*"       -ForegroundColor ($colors | get-random) -nonewline
+    Write-Host "*"       -ForegroundColor DarkGreen              -nonewline
+    Write-Host "*"       -ForegroundColor ($colors | get-random)
+    # Row Four
+    Write-Host "    *"   -ForegroundColor ($colors | get-random) -nonewline
+    Write-Host "*"       -ForegroundColor DarkGreen              -nonewline
+    Write-Host "*"       -ForegroundColor ($colors | get-random) -nonewline
+    Write-Host "*"       -ForegroundColor DarkGreen              -nonewline
+    Write-Host "*"       -ForegroundColor ($colors | get-random) -nonewline
+    Write-Host "*"       -ForegroundColor DarkGreen              -nonewline
+    Write-Host "*"       -ForegroundColor ($colors | get-random) -nonewline
+    Write-Host "*"       -ForegroundColor DarkGreen              -nonewline
+    Write-Host "*"       -ForegroundColor ($colors | get-random)
+    # Row Five
+    Write-Host "   *"    -ForegroundColor ($colors | get-random) -nonewline
+    Write-Host "*"       -ForegroundColor DarkGreen              -nonewline
+    Write-Host "*"       -ForegroundColor ($colors | get-random) -nonewline
+    Write-Host "*"       -ForegroundColor DarkGreen              -nonewline
+    Write-Host "*"       -ForegroundColor ($colors | get-random) -nonewline
+    Write-Host "*"       -ForegroundColor DarkGreen              -nonewline
+    Write-Host "*"       -ForegroundColor ($colors | get-random) -nonewline
+    Write-Host "*"       -ForegroundColor DarkGreen              -nonewline
+    Write-Host "*"       -ForegroundColor ($colors | get-random) -nonewline
+    Write-Host "*"       -ForegroundColor DarkGreen              -nonewline
+    Write-Host "*"       -ForegroundColor ($colors | get-random)
+    # Row Six
+    Write-Host "  *"     -ForegroundColor ($colors | get-random) -nonewline
+    Write-Host "*"       -ForegroundColor DarkGreen              -nonewline
+    Write-Host "*"       -ForegroundColor ($colors | get-random) -nonewline
+    Write-Host "*"       -ForegroundColor DarkGreen              -nonewline
+    Write-Host "*"       -ForegroundColor ($colors | get-random) -nonewline
+    Write-Host "*"       -ForegroundColor DarkGreen              -nonewline
+    Write-Host "*"       -ForegroundColor ($colors | get-random) -nonewline
+    Write-Host "*"       -ForegroundColor DarkGreen              -nonewline
+    Write-Host "*"       -ForegroundColor ($colors | get-random) -nonewline
+    Write-Host "*"       -ForegroundColor DarkGreen              -nonewline
+    Write-Host "*"       -ForegroundColor ($colors | get-random) -nonewline
+    Write-Host "*"       -ForegroundColor DarkGreen              -nonewline
+    Write-Host "*"       -ForegroundColor ($colors | get-random)
+    # Stump
+    Write-Host "       ***       " -ForegroundColor DarkGreen
+    Write-Host "       ***       " -ForegroundColor DarkGreen
+    Write-Host " Mutlu Yillar BÖTE"
+    $host.ui.RawUI.CursorPosition = $oldpos
+    sleep .40
+}
+Write-Host
