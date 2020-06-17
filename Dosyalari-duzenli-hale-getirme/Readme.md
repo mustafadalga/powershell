931ee1593dac3c31c5765b7746139788f042a9fe
Mustafa Dalga <mustafadalgaa@gmail.com>
2020-06-17 16:30:30 +0300

    Dosya ve klasörleri on yazma tarihlerine göre klasörleyen bir script tamamlandı

---
+++ Dosyalari-duzenli-hale-getirme/Readme.md
@@ -0,0 +1,4 @@
+# Dosyaları Düzenle Hale Getirme
+
+**Dosya ve klasörleri on yazma tarihlerine göre klasörleyen bir script.
+
---
+++ Dosyalari-duzenli-hale-getirme/script.ps1
@@ -0,0 +1,14 @@
+﻿Get-ChildItem .\  | foreach { 
+    $tarih = $_.LastWriteTime.ToShortDateString()
+    $klasorismi = Get-Date $tarih -Format yyyy.MM.dd
+    $DuzenliKlasor = ".\$klasorismi"
+    if (test-path $DuzenliKlasor)
+    { 
+        move-item $_.fullname $DuzenliKlasor 
+    }
+    else 
+    {
+      mkdir $DuzenliKlasor
+      move-item $_.fullname $DuzenliKlasor 
+    }
+}
