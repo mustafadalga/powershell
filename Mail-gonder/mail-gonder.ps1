2a67700aec3b9d1953984162cf8a34a7ad44dfb9
Mustafa Dalga <25087769+mustafadalga@users.noreply.github.com>
2020-06-18 01:58:20 +0300

    Update mail-gonder.ps1

--- Mail-gonder/mail-gonder.ps1
+++ Mail-gonder/mail-gonder.ps1
@@ -4,20 +4,20 @@ $smtpPort = "587"
 $gmail = new-object System.Net.Mail.SmtpClient($smtpSunucusu, $smtpPort)
 $gmail.EnableSsl = $true
 echo " "
-echo "Gmail E-posta ayarlarýnýz yapýlacak."
+echo "Gmail E-posta ayarlarÄ±nÄ±z yapÄ±lacak."
 echo " "
-echo "Kullandýðýnýz Gmail adresini giriniz."
+echo "KullandÄ±ÄŸÄ±nÄ±z Gmail adresini giriniz."
 $Global:kullanici =  read-host
 
-echo "Bu Gmail adresi için þifrenizi giriniz."
+echo "Bu Gmail adresi iÃ§in ÅŸifrenizi giriniz."
 $sifre = $host.UI.ReadLineAsSecureString()
 $gmail.credentials = new-object System.Net.NetworkCredential($kullanici, $sifre)
-add-member -InputObject $gmail -MemberType ScriptMethod -Name "gonder" -Value {param($kime, $konu, $govde); $gmail.send($kullanici, $kime, $konu, $govde); echo "Mail gönderildi"; }
+add-member -InputObject $gmail -MemberType ScriptMethod -Name "gonder" -Value {param($kime, $konu, $govde); $gmail.send($kullanici, $kime, $konu, $govde); echo "Mail gÃ¶nderildi"; }
 
 $Global:gmail = $gmail
 echo " "
-echo 'Mail ayarlarýnýz yapýldý. Mail gondermek için $gmail.gonder() komutunu kullanabilirsiniz.'
+echo 'Mail ayarlarÄ±nÄ±z yapÄ±ldÄ±. Mail gondermek iÃ§in $gmail.gonder() komutunu kullanabilirsiniz.'
 echo " "
-echo 'Örnek:'
+echo 'Ã–rnek:'
 echo '$gmail.gonder("alici@adre.si", "konu", "govdemetni")'
 echo " "
